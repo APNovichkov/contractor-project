227ef84266258e08dc5a84b6e9b3b7f03025efaa
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-03 14:55:13 -0700

    [Addition] Added add-to-cart functionality

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 client = MongoClient()
 db = client['contractor']
 products = db['products']
+cart = db['cart']
 
 app = Flask(__name__)
 
@@ -20,6 +21,15 @@ def index():
 def show_product(product_id):
     # print("Product: {}".format(products.find({'_id': product_id})))
     return render_template("product_show.html", product=products.find_one({'_id': ObjectId(product_id)}))
+
+@app.route("/store/<product_id>", methods=['POST'])
+def add_to_cart(product_id):
+    product_to_add = {
+        'product_id': product_id
+    }
+    cart.insert_one(product_to_add)
+    print("Inserted product_id into cart: {}".format(cart.find_one({'product_id': product_id})))
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
