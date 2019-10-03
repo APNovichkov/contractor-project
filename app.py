d7c88a10e9f3dcd3975484fe94a3227144c35705
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-03 15:55:22 -0700

    [fix] Cart is working now

--- app.py
+++ app.py
@@ -25,11 +25,22 @@ def show_product(product_id):
 @app.route("/store/<product_id>", methods=['POST'])
 def add_to_cart(product_id):
     product_to_add = {
-        'product_id': product_id
+        'product_id': product_id,
+        'name': products.find_one({'_id': ObjectId(product_id)})['name'],
+        'pic_id': products.find_one({'_id': ObjectId(product_id)})['pic_id'],
+        'price': products.find_one({'_id': ObjectId(product_id)})['price'],
+        'reviews_id': products.find_one({'_id': ObjectId(product_id)})['reviews_id'],
+        'description': products.find_one({'_id': ObjectId(product_id)})['description'],
+        'inventory': products.find_one({'_id': ObjectId(product_id)})['inventory']
     }
     cart.insert_one(product_to_add)
-    print("Inserted product_id into cart: {}".format(cart.find_one({'product_id': product_id})))
     return redirect(url_for('index'))
+
+@app.route("/store/cart")
+def show_cart():
+    return render_template("cart_show.html", cart_items=cart.find())
+
+
 
 
 if __name__ == "__main__":
