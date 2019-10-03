89d3e8fd35dbd8980a17674b320c4d400b30bd4e
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-03 11:12:07 -0700

    [Addition] Can display a single product and its attributes

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ def index():
     return render_template("inventory_show.html", products=products.find())
 
 
+@app.route("/store/<product_id>")
+def show_product(product_id):
+    # print("Product: {}".format(products.find({'_id': product_id})))
+    return render_template("product_show.html", product=products.find_one({'_id': ObjectId(product_id)}))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
