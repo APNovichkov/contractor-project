624068e910c31813a8dd106c16ea84ffb39029f8
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-06 17:14:47 -0700

    [addition] Added Heroku functionality

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-client = MongoClient()
-db = client['contractor']
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 products = db['products']
 cart = db['cart']
 
@@ -78,4 +79,4 @@ def cart_item_delete(product_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
