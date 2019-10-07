c6eda61300c4129e235d1802c04a41b8461eb309
Andrey Novichkov <andrey.novichkov@gmail.com>
2019-10-07 14:38:12 -0700

    [addition] Added functionality to leave reviews

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 products = db['products']
 cart = db['cart']
+reviews = db['reviews']
 
 st = SetupStore(products)
 st.populate_products()
@@ -46,6 +47,19 @@ def show_hoodies():
         cart_size=cart.count(),
         product_list=hoodies)
 
+@app.route("/store/review", methods=['POST'])
+def add_review():
+    print("HI!")
+
+    review = {
+        'product_id': request.form.get('product_id'),
+        'name': request.form.get('name'),
+        'content': request.form.get('content')
+    }
+
+    reviews.insert_one(review)
+    return redirect(url_for("show_product", product_id=review['product_id']))
+
 
 @app.route("/store/<product_id>")
 def show_product(product_id):
@@ -54,6 +68,8 @@ def show_product(product_id):
         "product_show.html",
         product=products.find_one({'_id': ObjectId(product_id)}),
         cart_size=cart.count(),
+        reviews=reviews.find({'product_id': product_id}),
+        num_of_reviews=reviews.find({'product_id': product_id}).count(),
         message=request.args.get("message"))
 
 @app.route("/store/<product_id>", methods=['POST'])
@@ -70,6 +86,7 @@ def add_to_cart(product_id):
     products.find_one_and_update({'_id': ObjectId(product_id)}, {'$inc': {'inventory': -1}})
     cart.insert_one(product_to_add)
     return redirect(url_for('show_product', product_id=product_id))
+
 
 @app.route("/store/cart")
 def show_cart():
